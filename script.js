a75aacdb80c8e8efd0e9ea2a89525a84acf0f5a7
Adi Meirovitch <adime.support@gmail.com>
2017-11-08 10:55:16 +0200

    adding return to the function!!!

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-/*function funcCalc(){
-    document.getElementById("firstNum").;
-    document.getElementById("secondNum")=num2;
-
-console.log(firstNum,secondNum);
-}*/
-
-  
-$(document).ready(function() {
-  $("#btnCalc").click(function() {
-    // Reading the data from the form
-    var words =($("#word").val());
-    var letter =($("#letter").val());
-    var count= fncCounter (words, letter);
-    document.getElementById("show").innerHTML = count;
-  });
-});
-   function fncCounter(words, letters) {
-    var counter=0;
-  for (var index = 0; index < words.length; index++){ 
-    if (letters= words.substring(index, index+1)) {counter++}  }
-
-
-}
-
+    $(document).ready(function() { 
+    $("#btnCalc").click(function() { 
+  var words =($("#word").val()); 
+  var letter =($("#letter").val()); 
+  var count= fncCounter (words, letter); 
+  document.getElementById("show").innerHTML = count; 
+ }); 
+}); 
+  function fncCounter(words, letters) { 
+   var counter=0; 
+ for (var index = 0; index < words.length; index++){  
+   if (letters=== words.substring(index, index+1)) {counter++}  } 
+   return counter;
+} 
+ 
